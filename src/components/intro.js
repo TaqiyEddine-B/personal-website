8745aac5907c57fc6e1bf7100a925e2e36082922
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2023-12-26 06:50:30 +0100

    Refactor intro.js component

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,10 +3,10 @@ import React from "react";
 const Intro = () => {
     return (
         <div>
-        <p> Hello, I'm</p>
-        <p>      Taqiy Eddine BOUKLOUHA </p>
-        <p> AI Developer @SolutionData Group | Toulouse, France</p>
+        <p> Hello, I'm  Taqiy Eddine BOUKLOUHA</p>
 
+        <p> AI Developer</p>
+        <p> Toulouse | France </p>
         <a href="https://www.linkedin.com/in/taqiyeddine-b/" target="_blank" className="beautiful-button" rel="noopener noreferrer">LinkedIn</a>
         <a href="https://github.com/taqiyeddine-b" target="_blank" className="beautiful-button" rel="noopener noreferrer">GitHub</a>
 
