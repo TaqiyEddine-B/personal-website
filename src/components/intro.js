e061dcf6c94a64a018558d9177bf04eef1b58464
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2023-12-26 07:11:24 +0100

    Update intro.js and global.css

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Intro = () => {
+    const greenTextStyle = {
+        color: '#007b5e',
+      };
     return (
         <div>
-        <p> Hello, <strong>I'm Taqiy Eddine BOUKLOUHA</strong></p>
+        <p>  Hello, I'm <strong><span style={greenTextStyle}>Taqiy Eddine BOUKLOUHA</span></strong> </p>
         <p className="job_title"> AI Developer</p>
         <p className="current_location"> Toulouse | France </p>
+
+        <p className="description"> Graduated in AI and software engineering, with enriching experience, my passion lies in the fields of Data Science,AI, and Machine/Deep Learning.<br></br>
+        Currently seeking full-time opportunities in these domains. </p>
 
         <a href="https://www.linkedin.com/in/taqiyeddine-b/" target="_blank" className="beautiful-button" rel="noopener noreferrer">LinkedIn</a>
         <a href="https://github.com/taqiyeddine-b" target="_blank" className="beautiful-button" rel="noopener noreferrer">GitHub</a>
