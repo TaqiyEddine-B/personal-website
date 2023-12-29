0c155454aa9e704dc61c6f044a60893702c66161
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2023-12-29 07:13:58 +0100

    Add useState hook and responsive font sizes to intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState} from "react";
 
 const Intro = () => {
     const greenTextStyle = {
         color: '#007b5e',
-      };
+        fontSize: '20px',
+    };
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
     return (
         <div>
-        <p>  Hello, I'm <strong><span style={greenTextStyle}>Taqiy Eddine BOUKLOUHA</span></strong> </p>
-        <p className="job_title"> AI Developer</p>
-        <p className="current_location"> Toulouse | France </p>
+        <p className="intro" style={{fontSize:isMobile ? '20px' :'24px'}}>   Hello, I'm <strong><span style={greenTextStyle}>Taqiy Eddine BOUKLOUHA</span></strong> </p>
+        <p className="intro" style={{fontSize:isMobile ? '18px' :'22px'}}> Artificial Intelligence Developer</p>
+        <p className="intro" style={{fontSize:isMobile ? '18px' :'22px'}}> Toulouse | France </p>
 
-        <p className="description"> Graduated in AI and software engineering, with enriching experience, my passion lies in the fields of Data Science,AI, and Machine/Deep Learning.<br></br>
+        <p className="intro" style={{fontSize:isMobile ? '18px' :'24px'}}> Graduated in AI and software engineering, with enriching experience, my passion lies in the fields of Data Science,AI, and Machine/Deep Learning.<br></br>
         Currently seeking full-time opportunities in these domains. </p>
 
         <a href="https://www.linkedin.com/in/taqiyeddine-b/" target="_blank" className="beautiful-button" rel="noopener noreferrer">LinkedIn</a>
