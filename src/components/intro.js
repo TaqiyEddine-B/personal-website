9639db349cdc9e4b0d9b915afa3061909fb44828
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2024-01-17 09:07:11 +0100

    Update styles and add new class for green text

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,16 +1,14 @@
 import React, { useState} from "react";
 import IconHolder from "./icon_holder";
+import "../styles/global.css";
 
 const Intro = () => {
-    const greenTextStyle = {
-        color: '#007b5e',
-        fontSize: '20px',
-    };
+
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
     return (
         <div>
-        <p className="intro" style={{fontSize:isMobile ? '20px' :'24px'}}>   Hello, I'm <strong><span style={greenTextStyle}>Taqiy Eddine BOUKLOUHA</span></strong> </p>
+        <p className="intro" style={{fontSize:isMobile ? '20px' :'24px'}}>   Hello, I'm <strong><span className="greenTextStyle">Taqiy Eddine BOUKLOUHA</span></strong> </p>
         <p className="intro" style={{fontSize:isMobile ? '18px' :'22px'}}> Artificial Intelligence Developer</p>
         <p className="intro" style={{fontSize:isMobile ? '18px' :'22px'}}> Toulouse | France </p>
 
